280226fe80b306a3413f61b57defe460271a92a0
Ethan Le <ethanle98@gmail.com>
2021-12-18 01:36:49 -0600

    Plan out variables and basic game loop Rename folder to team number and name. Delete example files and create script of minigame. Idea is to have player input WASD in a looping sequence and visualize it with an animated wheel and windows that slowly open in background, doing so fully if crankMax is reached. Sequence is instantiated as Array of Input strings.

---
+++ Assets/5 - Spicywater/CrankGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpicyWater
+{
+    public class CrankGame : MonoBehaviour
+    {
+        public Text UIText;
+        public string startText;
+        public string winText;
+        public int crankMax;
+        public int crankCurrent;
+        public float crankStep;
+        public int letterPos;
+        public string[] crankSequence = new string[]{ "W", "A", "S", "D" };
+        // Start is called before the first frame update
+        void Start()
+        {
+            UIText.text = startText;
+            MinigameManager.Instance.minigame.gameWin = false;
+            crankCurrent = 0;
+            if( crankMax != 0)
+                crankStep = 1 / crankMax;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(crankSequence[letterPos]))
+            {
+                if (letterPos >= crankSequence.Length)
+                    letterPos = 0;
+                else letterPos++;
+            }
+
+        }
+    }
+}
